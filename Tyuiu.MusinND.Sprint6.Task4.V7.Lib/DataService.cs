d3c6d56c41fa98da5520f411ad2322f22cdd1bb8
Nicki Hysterics <nicki.hysterics@gmail.com>
2025-01-25 03:12:10 +0500

    task4

---
+++ Tyuiu.MusinND.Sprint6.Task4.V7.Lib/DataService.cs
@@ -0,0 +1,47 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.MusinND.Sprint6.Task4.V7.Lib
+{
+    public class DataService : ISprint6Task4V7
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            // Список для хранения результатов
+            List<double> results = new List<double>();
+
+            // Табулирование функции
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double fx;
+
+                try
+                {
+                    // Проверяем деление на ноль (x + 2 = 0)
+                    if (x + 2 == 0)
+                    {
+                        fx = 0; // Возвращаем 0 при делении на ноль
+                    }
+                    else
+                    {
+                        // Вычисляем значение функции
+                        fx = Math.Cos(x) + (Math.Cos(x) / (x + 2)) - (3 * x);
+                    }
+                }
+                catch (DivideByZeroException)
+                {
+                    // В случае деления на ноль возвращаем 0
+                    fx = 0;
+                }
+
+                // Округляем результат до двух знаков после запятой
+                fx = Math.Round(fx, 2);
+
+                // Добавляем результат в список
+                results.Add(fx);
+            }
+
+            // Преобразуем список в массив и возвращаем
+            return results.ToArray();
+        }
+    }
+}
---
+++ Tyuiu.MusinND.Sprint6.Task4.V7.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tyuiu.MusinND.Sprint6.Task4.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
