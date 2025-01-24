7c4087ba6bed3cd2595448b7e2f50d3d51bc198f
Nicki Hysterics <nicki.hysterics@gmail.com>
2025-01-24 19:59:04 +0500

    task2

---
+++ Tyuiu.MusinND.Sprint6.Task2.V20.Lib/DataService.cs
@@ -0,0 +1,46 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.MusinND.Sprint6.Task2.V20.Lib
+{
+    public class DataService : ISprint6Task2V20
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            // Список для хранения значений функции
+            List<double> results = new List<double>();
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double fx;
+
+                try
+                {
+                    // Проверяем деление на ноль (x + 1.2 = 0)
+                    if (x + 1.2 == 0)
+                    {
+                        fx = 0; // Если деление на ноль, возвращаем 0
+                    }
+                    else
+                    {
+                        // Вычисляем значение функции
+                        fx = (Math.Sin(x) / (x + 1.2)) - (2 * Math.Sin(x)) - (2 * x);
+                    }
+                }
+                catch (DivideByZeroException)
+                {
+                    // Если возникает деление на ноль, возвращаем 0
+                    fx = 0;
+                }
+
+                // Округляем результат до двух знаков после запятой
+                fx = Math.Round(fx, 2);
+
+                // Добавляем результат в список
+                results.Add(fx);
+            }
+
+            // Преобразуем список в массив и возвращаем
+            return results.ToArray();
+        }
+    }
+}
---
+++ Tyuiu.MusinND.Sprint6.Task2.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MusinND.Sprint6.Task2.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MusinND.Sprint6.Task2.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f91c179f-4f59-4ef1-b8b3-4757a03c04a7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
