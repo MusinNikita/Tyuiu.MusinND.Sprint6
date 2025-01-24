e78ed8b7bb05e08c510052eb5f7bbbe02ae14a0d
Nicki Hysterics <nicki.hysterics@gmail.com>
2025-01-24 20:03:59 +0500

    task3

---
+++ Tyuiu.MusinND.Sprint6.Task3.V17.Lib/DataService.cs
@@ -0,0 +1,45 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.MusinND.Sprint6.Task3.V17.Lib
+{
+    public class DataService : ISprint6Task3V17
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            // Получаем количество строк и столбцов
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            if (rows != 5 || cols != 5)
+            {
+                throw new ArgumentException("Массив должен быть размером 5x5.");
+            }
+
+            // Преобразуем двумерный массив в массив строк
+            int[][] rowsArray = new int[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                rowsArray[i] = new int[cols];
+                for (int j = 0; j < cols; j++)
+                {
+                    rowsArray[i][j] = matrix[i, j];
+                }
+            }
+
+            // Сортируем строки по значениям в четвёртом столбце (индекс 3)
+            var sortedRows = rowsArray.OrderBy(row => row[3]).ToArray();
+
+            // Преобразуем обратно в двумерный массив
+            int[,] sortedMatrix = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    sortedMatrix[i, j] = sortedRows[i][j];
+                }
+            }
+
+            return sortedMatrix;
+        }
+    }
+}
---
+++ Tyuiu.MusinND.Sprint6.Task3.V17.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MusinND.Sprint6.Task3.V17.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MusinND.Sprint6.Task3.V17.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("517be9c9-c320-484b-99e1-4edc21fbb9fd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
