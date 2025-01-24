46c3aa1745fa85699d520c057138d7d28aae1f6f
Nicki Hysterics <nicki.hysterics@gmail.com>
2025-01-24 20:07:24 +0500

    task3

--- Tyuiu.MusinND.Sprint6.Task3.V17.Lib/DataService.cs
+++ Tyuiu.MusinND.Sprint6.Task3.V17.Lib/DataService.cs
@@ -9,17 +9,23 @@ namespace Tyuiu.MusinND.Sprint6.Task3.V17.Lib
             int rows = matrix.GetLength(0);
             int cols = matrix.GetLength(1);
 
-            // Проверка размера массива
+            // Проверка на размер 5x5
             if (rows != 5 || cols != 5)
                 throw new ArgumentException("Массив должен быть размером 5x5.");
 
-            // Преобразуем двумерный массив в массив строк для сортировки
-            var sortedRows = Enumerable.Range(0, rows)
-                .Select(i => Enumerable.Range(0, cols).Select(j => matrix[i, j]).ToArray())
-                .OrderBy(row => row[3]) // Сортируем по четвёртому столбцу
+            // Преобразуем двумерный массив в массив строк с индексами
+            var indexedRows = Enumerable.Range(0, rows)
+                .Select(i => (Index: i, Row: Enumerable.Range(0, cols).Select(j => matrix[i, j]).ToArray()))
                 .ToArray();
 
-            // Возвращаем отсортированный массив
+            // Сортируем строки по четвёртому столбцу, сохраняя исходный порядок для одинаковых значений
+            var sortedRows = indexedRows
+                .OrderBy(item => item.Row[3]) // Сортируем по четвёртому столбцу
+                .ThenBy(item => item.Index)  // Сохраняем порядок строк
+                .Select(item => item.Row)
+                .ToArray();
+
+            // Преобразуем обратно в двумерный массив
             int[,] result = new int[rows, cols];
             for (int i = 0; i < rows; i++)
                 for (int j = 0; j < cols; j++)
