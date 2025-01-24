760b7d84b56f835c10c9b1e056220057c86cd135
Nicki Hysterics <nicki.hysterics@gmail.com>
2025-01-24 20:18:34 +0500

    task3

--- Tyuiu.MusinND.Sprint6.Task3.V17.Lib/DataService.cs
+++ Tyuiu.MusinND.Sprint6.Task3.V17.Lib/DataService.cs
@@ -9,23 +9,19 @@ namespace Tyuiu.MusinND.Sprint6.Task3.V17.Lib
             int rows = matrix.GetLength(0);
             int cols = matrix.GetLength(1);
 
-            // Проверка на размер 5x5
-            if (rows != 5 || cols != 5)
-                throw new ArgumentException("Массив должен быть размером 5x5.");
-
-            // Создаём список строк, чтобы сохранить стабильность сортировки
-            var indexedRows = new (int Index, int[] Row)[rows];
+            // Копируем строки для сортировки
+            var rowsArray = new int[rows][];
             for (int i = 0; i < rows; i++)
             {
-                indexedRows[i] = (i, new int[cols]);
+                rowsArray[i] = new int[cols];
                 for (int j = 0; j < cols; j++)
                 {
-                    indexedRows[i].Row[j] = matrix[i, j];
+                    rowsArray[i][j] = matrix[i, j];
                 }
             }
 
-            // Сортируем по 4-му столбцу с учётом стабильности
-            Array.Sort(indexedRows, (x, y) => x.Row[3].CompareTo(y.Row[3]));
+            // Сортируем строки по 4-му столбцу с учётом стабильности
+            Array.Sort(rowsArray, (row1, row2) => row1[3].CompareTo(row2[3]));
 
             // Преобразуем обратно в двумерный массив
             int[,] result = new int[rows, cols];
@@ -33,7 +29,7 @@ namespace Tyuiu.MusinND.Sprint6.Task3.V17.Lib
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    result[i, j] = indexedRows[i].Row[j];
+                    result[i, j] = rowsArray[i][j];
                 }
             }
 
