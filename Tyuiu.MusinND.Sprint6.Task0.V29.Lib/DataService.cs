e55b1c2dc430e61aae5f0bf8695fe80963bfb2c8
Nicki Hysterics <nicki.hysterics@gmail.com>
2025-01-24 19:35:02 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.MusinND.Sprint6.Task0.V29.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.MusinND.Sprint6.Task0.V29.Lib
+{
+    public class DataService : ISprint6Task0V29
+    {
+        public double Calculate(int x)
+        {
+            double numerator = Math.Pow(x, 3) - 1;
+            double denominator = 4 * Math.Pow(x, 2);
+
+            double result = numerator / denominator;
+
+            return Math.Round(result, 3);
+        }
+    }
+}
---
+++ Tyuiu.MusinND.Sprint6.Task0.V29.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MusinND.Sprint6.Task0.V29.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MusinND.Sprint6.Task0.V29.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6679c949-b566-4e38-b20f-62540ad39c53")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
